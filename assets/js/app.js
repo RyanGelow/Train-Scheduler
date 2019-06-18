2a6954d7da65233e39a2e1c68de00c5147c8e44c
Ryan Gelow <exisphere@gmail.com>
2019-06-17 18:26:17 -0700

    html css and js all updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,80 +13,114 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
+
+let currentTime = $(`#time`).text(`${moment().format('h:mm')}`);
+
 let trainName = "";
-let destination = "";
+let currentStation = "";
+let firstTrainTime = "";
 let frequency = 0;
-let firstTrainTimeValue = ;
+
+let firstTrainTimeValue = moment().valueOf(firstTrainTime, 'hmm');
+let currentTimeValue = moment().valueOf()
+
+$(`#submit`).on('click', function() {
+  if($(`#train-name`).val() === "") {
+    $(`#train-name`).text("Please fill out form");
+    break;
+  }if($(`#current-station`).val() === "") {
+    $(`#current-station`).text("Please fill out form");
+    break;
+  }if($(`#train-time`).val() === "") {
+    $(`#train-time`).text("Please fill out form");
+    break;
+  }if($(`#train-frequency`).val() === "") {
+    $(`#train-frequency`).text("Please fill out form");
+    break;
+  }else{
+  trainName = $(`#train-name`).val();
+  console.log(trainName);
+  currentStation = $(`#current-station`).val();
+  console.log(currentStation);
+  firstTrainTime = $(`#train-time`).val();
+  console.log(firstTrainTime);
+  frequency = $(`#train-frequency`).val();
+  console.log(frequency);
+  console.log(firstTrainTimeValue);
+  console.log(currentTimeValue);
+  }
+});
+
 // could be 123 for 1:23 or 1234 for 12:34 HH:mm is military time
 // parseInt(moment('12:34','HH:mm').format('hmm')); === 1234
 
-let firstTrainTimeFormat = moment(, "hmm").format("HH:mm")
-let firstTrainTimeValue = moment().valueOf(firstTrainTime, 'hmm');
-let firstTrainTime = 0;
-let currentTimeFormat = moment().format('HH:mm');
-let currentTimeValue = moment().valueOf()
-let nextArrival = (currentTimeValue - firstTrainTimeValue)/frequency 
-let minutesAway = 
+
+// let firstTrainTimeFormat = moment("hmm").format("HH:mm")
+// let firstTrainTimeValue = moment().valueOf(firstTrainTime, 'hmm');
+// let currentTimeFormat = moment().format('HH:mm');
+// let currentTimeValue = moment().valueOf()
+// let nextArrival = (currentTimeValue - firstTrainTimeValue)/frequency 
+// let minutesAway = 
 
 
-database.ref().on("value", function(snapshot) {
-    if (snapshot.child("trainName").exists() && snapshot.child("destination").exists() && snapshot.child("firstTrainTime").exists() && snapshot.child("frequency").exists()) {
+// database.ref().on("value", function(snapshot) {
+//     if (snapshot.child("trainName").exists() && snapshot.child("destination").exists() && snapshot.child("firstTrainTime").exists() && snapshot.child("frequency").exists()) {
 
-        trainName = snapshot.val().trainTime;
-        destination = snapshot.val().destination;
-        frequency = snapshot.val().frequency;
-        firstTrainTime = snapshot.val().firstTrainTime;
-        nextArrival = trainName
+//         trainName = snapshot.val().trainTime;
+//         destination = snapshot.val().destination;
+//         frequency = snapshot.val().frequency;
+//         firstTrainTime = snapshot.val().firstTrainTime;
+//         nextArrival = trainName
 
-    }
-})
+//     }
+// })
 
 
 
-database.ref().on("child_added", function(snapshot) {
+// database.ref().on("child_added", function(snapshot) {
 
-})
+// })
 
-        // Capture Button Click
-    $("#add-user").on("click", function(event) {
-        event.preventDefault();
+//         // Capture Button Click
+//     $("#add-user").on("click", function(event) {
+//         event.preventDefault();
   
-        // Grabbed values from text boxes
-        name = $("#name-input").val().trim();
-        email = $("#email-input").val().trim();
-        age = $("#age-input").val().trim();
-        comment = $("#comment-input").val().trim();
+//         // Grabbed values from text boxes
+//         name = $("#name-input").val().trim();
+//         email = $("#email-input").val().trim();
+//         age = $("#age-input").val().trim();
+//         comment = $("#comment-input").val().trim();
   
-        // Code for handling the push
-        database.ref().push({
-          name: name,
-          email: email,
-          age: age,
-          comment: comment,
-          dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
+//         // Code for handling the push
+//         database.ref().push({
+//           name: name,
+//           email: email,
+//           age: age,
+//           comment: comment,
+//           dateAdded: firebase.database.ServerValue.TIMESTAMP
+//         });
   
-      });
+//       });
   
-      // Firebase watcher .on("child_added"
-      database.ref().on("child_added", function(snapshot) {
-        // storing the snapshot.val() in a variable for convenience
-        var sv = snapshot.val();
+//       // Firebase watcher .on("child_added"
+//       database.ref().on("child_added", function(snapshot) {
+//         // storing the snapshot.val() in a variable for convenience
+//         var sv = snapshot.val();
   
-        // Console.loging the last user's data
-        console.log(sv.name);
-        console.log(sv.email);
-        console.log(sv.age);
-        console.log(sv.comment);
+//         // Console.loging the last user's data
+//         console.log(sv.name);
+//         console.log(sv.email);
+//         console.log(sv.age);
+//         console.log(sv.comment);
   
-        // Change the HTML to reflect
-        $("#name-display").text(sv.name);
-        $("#email-display").text(sv.email);
-        $("#age-display").text(sv.age);
-        $("#comment-display").text(sv.comment);
+//         // Change the HTML to reflect
+//         $("#name-display").text(sv.name);
+//         $("#email-display").text(sv.email);
+//         $("#age-display").text(sv.age);
+//         $("#comment-display").text(sv.comment);
   
-        // Handle the errors
-      }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      });
+//         // Handle the errors
+//       }, function(errorObject) {
+//         console.log("Errors handled: " + errorObject.code);
+//       });
   
